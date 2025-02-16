d2516c4cc132c699ec98f5eeb9346800c816d18e
Shopen <hosein.shorabi@live.com>
2025-02-16 15:33:44 +0330

    add git ignore

--- Assignment-03/assignment3-3.c
+++ Assignment-03/assignment3-3.c
@@ -6,10 +6,9 @@ int main(){
 	char yourname[40];
 	
 	printf("Type your name and lastname with space?\n");
-	
-	fgets(yourname, sizeof(yourname), stdin);
-	
-	printf("fgets Accept space, so your name is: %40s",yourname);
+
+	fgets(yourname, sizeof(yourname), stdin);	
+	printf("fgets Accept space, so your name is: %s",yourname);
 	getchar(); // wait to get a char then continue... I used it to see result before exit.
 	return 0;	
 }
